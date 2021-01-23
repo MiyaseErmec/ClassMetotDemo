5a5a781dcc05c0f6f5c72e24f4924e50facbba36
Miyase ERMEÇ <esra_miyase@outlook.com>
2021-01-23 20:47:23 +0300

    homework

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşterinin maaş bilgisi eklendi.");
+            Console.WriteLine(musteri.Maasi + " TL" );
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşterilerin maaş bilgisi silindi.");
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Maasi + " TL ");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id      = 1;
+            musteri1.Adi     = "Miyase";
+            musteri1.Soyadi  = "Ermeç";
+            musteri1.Meslegi = "Tekstil Laborant";
+            musteri1.Maasi   = 2500;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Kerem";
+            musteri2.Soyadi = "Aslan";
+            musteri2.Meslegi = "Mühendis";
+            musteri2.Maasi = 5000;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Ayaz";
+            musteri3.Soyadi = "Tanrıkulu";
+            musteri3.Meslegi = "Doktor";
+            musteri3.Maasi = 15000;
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3};
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Adı : "+ musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı : "+ musteri.Soyadi);
+                Console.WriteLine("Mesleği : " + musteri.Meslegi);
+                Console.WriteLine("Maaşı : " + musteri.Maasi + " TL");
+            }
+
+            Console.WriteLine(".................................................");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Sil(musteri1);
+            Console.WriteLine("Müşterilerin Maaş Listesi ;");
+            musteriManager.List(musteriler);
+      
+            
+
+
+
+
+
+
+
+        }
+    }
+}
